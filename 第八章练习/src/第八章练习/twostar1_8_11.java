2121f62f9dc2144a94e08bb9017a627db97b0a71
WAguai <77195089+WAguai@users.noreply.github.com>
2022-01-03 15:15:30 +0800

    Add files via upload

---
+++ 第八章练习/src/第八章练习/twostar1_8_11.java
@@ -0,0 +1,16 @@
+package 第八章练习;
+import java.util.Scanner;
+import java.util.List;
+import java.util.LinkedList;
+public class twostar1_8_11 {
+	static public void main(String[]args) {
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		System.out.printf("输入一个0到511之间的数字");
+		int x=n;
+		char [][]a;
+		while(n>0) {
+			
+		}
+	}
+}
